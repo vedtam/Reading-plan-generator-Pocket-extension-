a46e36ddf937bcdf478a20e36d4697793cd122c6
Edmond Varga <edmond.varga@aperio-intelligence.com>
2023-04-10 15:58:23 +0300

    fix

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 import * as pushController from './controller/push.js';
-import fs from 'fs';
-import https from 'https';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(path.join(process.cwd(), 'client')));
@@ -18,12 +16,4 @@ router.route('/subscribe')
 
 app.use('/', router);
 
-// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-https.createServer({
-  key: fs.readFileSync('ssl/2334cd2e0ad1388a.pem'),
-  cert: fs.readFileSync('ssl/2334cd2e0ad1388a.crt'),
-  ca: fs.readFileSync('ssl/gd_bundle-g2-g1.crt')
-}, app).listen(app.locals.port, function() {
- console.log("Started on PORT " + app.locals.port);
-});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
